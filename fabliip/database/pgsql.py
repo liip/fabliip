19b58defe397a01d3abf0f13985839894add81fc
Sylvain Fankhauser <sylvain.fankhauser@liip.ch>
2014-01-10 15:19:54 +0100

    pgsql: make password prompt optional if it's supplied to the backup function

--- fabliip/database/pgsql.py
+++ fabliip/database/pgsql.py
@@ -3,7 +3,8 @@ from getpass import getpass
 from fabric import api
 
 
-def dump(backup_path, database_name, user='postgres', host=None):
+def dump(backup_path, database_name, user='postgres', host=None,
+         password=None):
     """
     Backs up the given database to the given file as a PostgreSQL archive. If
     host is set to None, a local connection will be used, so you'll need to be
@@ -17,10 +18,11 @@ def dump(backup_path, database_name, user='postgres', host=None):
                      backup_path=backup_path,
                  ), user=user)
     else:
-        db_password = getpass('Enter database password for {user}: '
-                              .format(user=user))
+        if password is None:
+            password = getpass('Enter database password for {user}: '
+                                  .format(user=user))
 
-        with api.shell_env(PGPASSWORD=db_password):
+        with api.shell_env(PGPASSWORD=password):
             api.run('pg_dump -Fc -U {user} -h {host} {database_name} >'
                     ' {backup_path}'
                     .format(
